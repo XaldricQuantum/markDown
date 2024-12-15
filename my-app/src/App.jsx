635d6d22be6fb04951e883bf33253f28f88b4fb5
saeid <s_monfared1@yahoo.com>
2024-12-16 00:46:36 +0300

    fix: DOM elements undefined

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,33 +5,66 @@ import './App.css'
 import gsap from 'gsap'
 
 const PLACEHOLDER = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n";
-
+let editor, editorWrapper, preview, previewWrapper;
+window.onload = () => {
+  
+  editor = document.getElementById("editor");
+  editorWrapper = document.getElementById("editor-wrapper");
+  preview = document.getElementById("preview");
+  previewWrapper = document.getElementById("preview-wrapper")
+  document.getElementById("editor").value = PLACEHOLDER;
+  // handleChange();
+}
 
 function App() {
   const [maximized, setMaximized] = useState(null) // (null) , "editor", "preview"
+  // window.onload = () => {
+  
+  //   editor = document.getElementById("editor");
+  //   editorWrapper = document.getElementById("editor-wrapper");
+  //   preview = document.getElementById("preview");
+  //   previewWrapper = document.getElementById("preview-wrapper")
+  //   document.getElementById("editor").value = PLACEHOLDER;
+  //   handleChange();
+  // }
+  console.log(editor)
   // const []
+  
+  marked.setOptions({
+    breake: true,
+    highlight: function(e) {
+      return Prism.highlight(e, Prism.languages.javascript, "javascript")
+    }
+  })
+
+
   const handleOnClick =  (window) => {
     setMaximized((prev) => prev === window ? null : window);
     console.log(maximized)
   }
 
   const handleChange = () => {
-    console.log("Handle Cgange Here")
+    console.log("Handle Cgange Here");
+    console.log(preview);
+    console.log(editor);
+    document.getElementById("preview").innerHTML = window.marked.parse(document.getElementById("editor").value)
   }
+
+  
 
   useEffect(() => {
     if (maximized === "editor") {
-      gsap.to(document.getElementById("editor"), {
+      gsap.to(editor, {
         height: "100vh",
         duration: 1,
         ease: "power1.inOut"
       });
-      console.log(document.getElementById("editor-wrapper"))
-      gsap.to(document.getElementById("preview-wrapper"), {
+      console.log(editorWrapper)
+      gsap.to(previewWrapper, {
         opacity: 0,
         duration: 1,
         onComplete: () => {
-          document.getElementById("preview-wrapper").style.display='none' ;
+          previewWrapper.style.display='none' ;
         }
       });
     } else if (maximized === "preview") {
@@ -53,6 +86,7 @@ function App() {
       gsap.to(document.getElementById("editor-wrapper"), {opacity: 1, scale: 1, duration: 1, ease: "power1.inOut"})
       gsap.to(document.getElementById("editor"), {height: "45vh",})
       gsap.to(document.getElementById("preview-wrapper"), {opacity: 1, scale: 1, duration: 1, ease: "power1.inOut"})
+      // setTimeout(handleChange(), 1000);
     }
   }, [maximized])
 
@@ -64,7 +98,7 @@ function App() {
             icon={maximized === "editor" ? 'fa fa-compress' : 'fa-solid fa-maximize'} 
             onClick={() => handleOnClick('editor')} 
           />
-          <textarea id='editor' placeholder={PLACEHOLDER} onChange={handleChange}/>
+          <textarea id='editor' onChange={handleChange}/>
         </div>
         <div className='preview-wrapper' id='preview-wrapper'>
           <Toolbar 
